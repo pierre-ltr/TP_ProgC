34686adb493c0c0da3ba1730cb30cfa820a4725b
Pierro <160888578+pierre-ltr@users.noreply.github.com>
2026-09-18 09:14:27 +0000

    Exercice 1.5

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    int a = 16;
+    int b = 3;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    printf("Addition: %d\n", a + b);
+    printf("Soustraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    printf("Modulo: %d\n", a % b);
+
+    bool est_egal = (a == b);
+    bool est_superieur = (a > b);
+
+    printf("a == b : %d\n", est_egal);
+    printf("a > b : %d\n", est_superieur);
+
+    return 0;
+}
 
 
