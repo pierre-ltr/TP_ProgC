89726b5dcaa63e52c8b2c89ec995ac75c3ac3772
Pierro <160888578+pierre-ltr@users.noreply.github.com>
2026-09-18 09:14:47 +0000

    Exercice 1.8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1 = 20;
+    int num2 = 5;
+    char op = '+';
+
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Operateur inconnu\n");
+            break;
+    }
+
+    return 0;
+}
 
