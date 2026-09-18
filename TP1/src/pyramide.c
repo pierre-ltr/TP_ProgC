490acd4e9bfc5b359616c89deb4f00634121c3c0
Pierro <160888578+pierre-ltr@users.noreply.github.com>
2026-09-18 09:14:49 +0000

    Exercice 1.10

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        for (int espaces = 1; espaces <= n - i; espaces++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+    return 0;
+}
 
